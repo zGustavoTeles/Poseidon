928b67d20ada0a519da69823c88e5b174e2fda63
Gustavo Mateus <gustavo@artnew.com.br>
2022-01-28 17:08:23 -0300

    :boom: implementando o quagajs

--- src/app/pages/cadastro-produtos/cadastro-produtos.page.ts
+++ src/app/pages/cadastro-produtos/cadastro-produtos.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AlertController, Config } from '@ionic/angular';
 import { FirebaseService } from '../../firebase.service';
 import { DadosRepositories } from '../../providers/dados-repositories';
 import { Base64ImgUtil } from '../util/base64img.util';
+import Quagga from 'quagga';
 
 @Component({
     selector: 'app-cadastro-produtos',
@@ -42,12 +43,39 @@ export class CadastroTipoProdutoPage implements OnInit {
     senha: any;
     nome: any;
 
+    barcode = '';
+    barcodeResult;
+    configQuagga = {
+        inputStream: {
+            name: 'Live',
+            type: 'LiveStream',
+            target: '#inputBarcode',
+            constraints: {
+                width: { min: 640 },
+                height: { min: 480 },
+                aspectRatio: { min: 1, max: 2 }, // sane aspect ratios?
+                facingMode: 'environment', // or user
+            },
+            singleChannel: false // true: only the red color-channel is read
+        },
+        locator: {
+            patchSize: 'medium',
+            halfSample: true
+        },
+        locate: true,
+        numOfWorkers: 4,
+        decoder: {
+            readers: ['code_128_reader']
+        }
+    };
+
     constructor(
         private firebaseService: FirebaseService,
         public config: Config,
         private alertController: AlertController,
         private dadosRepositories: DadosRepositories,
         private base64ImgUtil: Base64ImgUtil,
+        private ref: ChangeDetectorRef
     ) { }
 
     ngOnInit() {
@@ -199,10 +227,72 @@ export class CadastroTipoProdutoPage implements OnInit {
         this.sexo = this.dadosRepositories.getLocalStorage('sexo');
     }
 
-    /**
-    * Pegando o código de barras
-    */
-    async getBarCode() {
+    testChangeValues() {
+        this.barcode = 'Code-barres bidon : 0123456789';
+    }
 
+    startScanner() {
+        this.barcode = '';
+        this.ref.detectChanges();
+
+        Quagga.onProcessed((result) => this.onProcessed(result));
+
+        Quagga.onDetected((result) => this.logCode(result));
+
+        Quagga.init(this.configQuagga, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            Quagga.start();
+            console.log('Barcode: initialization finished. Ready to start');
+        });
+
+
+    }
+
+    private onProcessed(result: any) {
+        const drawingCtx = Quagga.canvas.ctx.overlay;
+        const drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute('width'), 10), parseInt(drawingCanvas.getAttribute('height'), 10));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: 'green', lineWidth: 2 });
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: '#00F', lineWidth: 2 });
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+            }
+        }
+    }
+
+    private logCode(result) {
+        const code = result.codeResult.code;
+
+        if (this.barcode !== code) {
+            this.barcode = 'Code-barres EAN : ' + code;
+            this.barcodeResult = result.codeResult;
+            this.ref.detectChanges();
+            console.log(this.barcode);
+            console.log(this.barcodeResult);
+
+            // this.barcodeValue = result.codeResult.code;
+            // this.barcodeResult=result.codeResult
+            // console.log("this.barcodeValue",this.barcodeValue)
+
+            console.log("JSON.stringify(result.codeResult)", JSON.stringify(result.codeResult))
+            console.log("Result", result)
+            console.log("JSON.stringify(result)", JSON.stringify(result))
+            // console.log("this.barcodeResult",this.barcodeResult.json())
+            Quagga.stop();
+        }
     }
 }
